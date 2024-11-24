5390b305744e2b250bb1177e6324e7ed67608d70
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-11-24 20:54:04 -0300

    chore: change the link to the resume

--- src/components/Introduce/ResumeLink.vue
+++ src/components/Introduce/ResumeLink.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    href="#"
+    href="https://drive.google.com/file/d/1tVcEfCvM4f9TgyueXR8xaexgFa1YX0Jb/view?usp=sharing"
     target="_blank"
     rel="noreferrer"
     class="jetbrains-mono flex items-center gap-2.5 rounded-lg bg-background px-5 py-2.5 text-xs uppercase text-white active:bg-black dark:bg-white dark:text-black dark:active:bg-background dark:active:text-white"
